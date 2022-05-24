248508f0625707d3c6d788663a1bb319574802bf
Malcolm Hendricks <redwindmh@gmail.com>
2022-05-25 00:16:41 +0900

    Major graphics and map progress

--- tile.py
+++ tile.py
@@ -1,9 +1,13 @@
 import pygame
 from settings import *
 
+
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(
+        self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))
+    ):
         super().__init__(groups)
-        self.image = pygame.image.load('./images/Tilesets/rock.png')
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-10)
+        self.sprite_type = sprite_type
+        self.image = surface
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
