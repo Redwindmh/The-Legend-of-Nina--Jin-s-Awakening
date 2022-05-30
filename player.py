b8f212b4f52dce1b69885a0faba9f7d97bbf0ab6
Malcolm Hendricks <redwindmh@gmail.com>
2022-05-30 09:43:21 +0900

    Map collision improvements

--- player.py
+++ player.py
@@ -1,21 +1,50 @@
 import pygame
 from settings import *
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,obstable_sprites):
-        super().__init__(groups)
-        self.image = pygame.image.load('./images/BlueSamurai/player.png')
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-26)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load("./images/BlueSamurai/player.png")
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
+
+        # graphics setup
+        self.import_player_assets()
+
+        # movement
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
-        self.obstacle_sprites = obstable_sprites
+        self.obstacle_sprites = obstacle_sprites
+
+    def import_player_assets(self):
+        character_path = "./images/sprites/jin"
+        self.animations = {
+            "up": [],
+            "down": [],
+            "left": [],
+            "right": [],
+            "up_idle": [],
+            "down_idle": [],
+            "left_idle": [],
+            "right_idle": [],
+            "up_attack": [],
+            "down_attack": [],
+            "left_attack": [],
+            "right_attack": [],
+        }
+
+        for animation in self.animations.keys():
+            print(animation)
 
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # movement input
         if keys[pygame.K_UP]:
             self.direction.y = -1
         elif keys[pygame.K_DOWN]:
@@ -30,18 +59,30 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        # attack input
+        if keys[pygame.K_SPACE] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print("attack!")
+
+        # magic input
+        if keys[pygame.K_LSHIFT] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print("magic!")
+
     def move(self, speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
         self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
+        self.collision("horizontal")
         self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
+        self.collision("vertical")
         self.rect.center = self.hitbox.center
 
-    def collision(self,direction):
-        if direction == 'horizontal':
+    def collision(self, direction):
+        if direction == "horizontal":
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0:
@@ -57,6 +98,14 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0:
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
+
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
+
     def update(self):
         self.input()
+        self.cooldowns()
         self.move(self.speed)
