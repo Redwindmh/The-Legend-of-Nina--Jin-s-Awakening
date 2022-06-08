f1d923a5c19f839987208e30a27861673ef60020
Malcolm Hendricks <redwindmh@gmail.com>
2022-06-08 16:46:52 +0900

    Began work on weapons

--- level.py
+++ level.py
@@ -6,6 +6,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice, random
+from weapon import Weapon
 
 
 class Level:
@@ -66,8 +67,14 @@ class Level:
         #             )
 
         self.player = Player(
-            (2000, 1450), [self.visible_sprites], self.obstacle_sprites
+            (2000, 1450),
+            [self.visible_sprites],
+            self.obstacle_sprites,
+            self.create_attack,
         )
+
+    def create_attack(self):
+        Weapon(self.player, [self.visible_sprites])
 
     def run(self):
         # update and draw the game
