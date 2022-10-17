d2caaeb20b10b83851c6e730c6c76b4a8c27f51f
Malcolm Hendricks <redwindmh@gmail.com>
2022-10-17 17:28:18 +0900

    Continued work on enemy logic

--- enemy.py
+++ enemy.py
@@ -2,14 +2,18 @@ import pygame
 from settings import *
 from entity import Entity
 
+
 class Enemy(Entity):
     def __init__(self, enemy_name, pos, groups):
 
         # General setup
         super().__init__(groups)
-        self.sprite_type = 'enemy'
+        self.sprite_type = "enemy"
 
         # Graphics setup
         self.import_graphics(enemy_name)
-        self.image = pygame.Surface((64,64))
-        self.rect = self.image.get_rect(topleft = pos)
+        self.image = pygame.Surface((64, 64))
+        self.rect = self.image.get_rect(topleft=pos)
+
+    def import_graphics(self, name):
+        self.animations = {"idle": [], "move": [], "attack": []}
