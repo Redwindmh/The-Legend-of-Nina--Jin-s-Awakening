9206049f7093a087dbfd68138ecfd496b7477d0c
Malcolm Hendricks <redwindmh@gmail.com>
2023-02-14 12:57:08 +0900

    Added particle effects

--- level.py
+++ level.py
@@ -9,6 +9,7 @@ from random import choice, random
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
+from particles import AnimationPlayer
 
 
 class Level:
@@ -29,6 +30,9 @@ class Level:
 
         # user interface
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer()
 
     def create_map(self):
         layouts = {
@@ -99,7 +103,8 @@ class Level:
                                     enemy_name,
                                     (x, y),
                                     [self.visible_sprites, self.attackable_sprites],
-                                    self.obstacle_sprites, self.damage_player
+                                    self.obstacle_sprites,
+                                    self.damage_player,
                                 )
 
         #         if col == "x":
@@ -138,6 +143,7 @@ class Level:
                             target_sprite.get_damage(
                                 self.player, attack_sprite.sprite_type
                             )
+
     def damage_player(self, amount, attack_type):
         if self.player.vulnerable:
             self.player.health -= amount
--- particles.py
+++ particles.py
@@ -1,8 +1,45 @@
 import pygame
 from support import import_folder
 
+
 class ParticleEffect(pygame.sprite.Sprite):
-    def __init__(self,pos,animation_frames,groups):
+    def __init__(self):
+        self.frames = {
+            # magic
+            "flame": import_folder("../images/graphics/particles/flame/frames"),
+            "aura": import_folder("../images/graphics/particles/aura"),
+            "heal": import_folder("../images/graphics/particles/heal/frames"),
+            # attacks
+            "claw": import_folder("../images/graphics/particles/claw"),
+            "slash": import_folder("../images/graphics/particles/slash"),
+            "sparkle": import_folder("../images/graphics/particles/sparkle"),
+            "leaf_attack": import_folder("../images/graphics/particles/leaf_attack"),
+            "thunder": import_folder("../images/graphics/particles/thunder"),
+            # monster deaths
+            "squid": import_folder("../images/graphics/particles/smoke_orange"),
+            "raccoon": import_folder("../images/graphics/particles/raccoon"),
+            "spirit": import_folder("../images/graphics/particles/nova"),
+            "bamboo": import_folder("../images/graphics/particles/bamboo"),
+            # leafs
+            "leaf": (
+                import_folder("../images/graphics/particles/leaf2"),
+                import_folder("../images/graphics/particles/leaf1"),
+                import_folder("../images/graphics/particles/leaf3"),
+                import_folder("../images/graphics/particles/leaf4"),
+                import_folder("../images/graphics/particles/leaf5"),
+                import_folder("../images/graphics/particles/leaf6"),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf1")),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf2")),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf3")),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf4")),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf5")),
+                self.reflect_images(import_folder("../images/graphics/particles/leaf6")),
+            ),
+        }
+
+    def reflect_images(self):
+
+    def __init__(self, pos, animation_frames, groups):
         super().__init__(groups)
         self.frame_index = 0
         self.animation_speed = 0.15
